4dc9cb0a41535bfbfae1f8db6dfea2f916c88828
Rakesh Ravi Shankar <Rakesh@dhcp-18-111-5-232.dyn.mit.edu>
2017-10-07 16:44:26 -0400

    Boilerplate

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	public static bool modelInstantiated;
+
+	// Use this for initialization
+	void Start () {
+		modelInstantiated = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour {
+
+	public void closeView () {
+		GameObject currentModel = GameObject.FindGameObjectWithTag ("CurrentModel");
+		if (currentModel != null) {
+			GameObject.Destroy (currentModel);
+		}
+		GameManager.modelInstantiated = false;
+	}
+}
