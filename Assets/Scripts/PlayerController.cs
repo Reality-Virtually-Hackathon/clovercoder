f1042ec8c315bd5c8b91adba0dda1bcafdba5b9d
Rohan Sapre <sapre.r@husky.neu.edu>
2017-10-08 11:17:29 -0400

    networking working now for pinch to zoom and game object changes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,34 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using System.Collections;
 
 public class PlayerController : NetworkBehaviour {
+	
+	void Start () {
+	}
     
     // Update is called once per frame
     void Update () {
-		var x = Input.GetAxis ("Horizontal") * Time.deltaTime * 150.0f;
-		var z = Input.GetAxis ("Vertical") * Time.deltaTime * 3.0f;
+		if (Input.GetMouseButtonDown (0)) {
+			transform.localScale -= new Vector3 (0.1f, 0.1f, 0.1f);
+		}
+		if (Input.GetMouseButtonDown (1)) {
+			GetComponent<Renderer>().material.color = Color.blue;
+			if (isServer) {
+				RpcChanges ();
+			} else {
+				CmdUpdate ();
+			}
+		}
+	}
 
-		transform.Rotate (0, x, 0);
-		transform.Translate (0, 0, z);
+	[Command]
+	void CmdUpdate () {
+		GetComponent<Renderer>().material.color = Color.blue;
+	}
+
+	[ClientRpc]
+	void RpcChanges() {
+		GetComponent<Renderer>().material.color = Color.blue;
 	}
 }
